e224413ee3085c020be3e784f166047e75d7fa0d
zTjamm <jamm85@bk.ru>
2022-06-14 12:16:04 +0300

    12
    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import Profile from "./components/profile/Profile";
-import user from "./components/profile/user.json";
-import Statistics from "./components/statistics/Statistic";
-import data from "./components/statistics/data.json";
-import FriendsList from "./components/friend-list/FriendsList";
-import friends from "./components/friend-list/friends.json";
-import TransHistory from './components/transaction-history/TransHistory';
-import transactions from './components/transaction-history/transactions.json';
-import { GlobalStyles } from "./GlobalStyle";
-import { Global } from "@emotion/react";
+import React from 'react';
 
 export default function App() {
   return (
       <div>
-      <Global styles={GlobalStyles} />
-      <Profile
-        userName={user.username}
-        tag={user.tag}
-        place={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics stats={data} title={'Upload stats'} />;         
-      <Statistics stats={data} />;
-      <FriendsList friends={friends}  />
-      <TransHistory transactions={transactions}/>                
+      text fo my first
     </div>
   );
 }
